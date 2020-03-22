6633ca7d08ad2e1ffe22fc35ce2fc720d96d6011
Daniel Milo <danielmilo12@gmail.com>
2020-03-22 23:33:06 +0200

    audio, terrain

---
+++ Assets/Scripts/AudioTransitionCaller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTransitionCaller : MonoBehaviour
+{
+    private AudioTransitionController controller;
+
+    [SerializeField] string SnapshotName;
+    [SerializeField] float TransitionLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.FindGameObjectWithTag("AudioController").GetComponent<AudioTransitionController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("AudioTransitionCaller::OnCollisionEnter");
+        if (other.gameObject.tag == "Player")
+        {
+            controller.TransitionTo(SnapshotName, TransitionLength);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AudioTransitionController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioTransitionController : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mixer != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                mixer.FindSnapshot("Initial").TransitionTo(3f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                mixer.FindSnapshot("1-1").TransitionTo(3f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                mixer.FindSnapshot("2-1").TransitionTo(3f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                mixer.FindSnapshot("3-1").TransitionTo(3f);
+            }
+        }
+    }
+
+    public void TransitionTo(string name, float time)
+    {
+        mixer.FindSnapshot(name).TransitionTo(time);
+    }
+}
