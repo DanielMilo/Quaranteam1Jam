c25d3bc2551a51c641313d3bac332ef5be62d56a
noobay <adargro@gmail.com>
2020-03-22 20:46:13 +0200

    Added a base for collectibles. Added 3 different collectible prefabs. Added an inventory System

---
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Collectibles are In-Scene proxies for items.
+/// </summary>
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Collider))]
+public class Collectible : MonoBehaviour
+{
+    private static string PLAYER_TAG = "Player";
+
+    [SerializeField]
+    private Item m_associatedItem;
+
+    private void Awake()
+    {
+        // make sure the collider is set to trigger from the editor, this method is mostly full-proof. 
+        // NOTE: you should remove this logic and if you need more than 1 collider on the GameObject.
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PLAYER_TAG))
+        { 
+            Inventory.Instance.AddItem(m_associatedItem);
+        }
+    }
+}
---
+++ Assets/Scripts/Collectibles/Item.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum EItemType
+{
+    ITEM1 = 0,
+    ITEM2 = 1,
+    ITEM3 = 2
+}
+
+[CreateAssetMenu(fileName = "Collectible", menuName ="Level Authoring/Collectible")]
+public class Item : ScriptableObject
+{
+    [SerializeField]
+    private EItemType m_item;
+
+    [SerializeField]
+    private Texture m_texture;
+
+    public EItemType ItemType
+    {
+        get
+        {
+            return m_item;
+        }
+    }
+
+    public Texture Texture
+    {
+        get
+        {
+            return m_texture;
+        }
+    }
+}
