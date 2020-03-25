dd79b33f48a83aa2311a7100253d5519eb59f86b
Daniel Milo <danielmilo12@gmail.com>
2020-03-25 19:43:15 +0200

    collider scripts

---
+++ Assets/Scripts/PlaySoundOnCollide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
+public class PlaySoundOnCollide : MonoBehaviour
+{
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            source.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
